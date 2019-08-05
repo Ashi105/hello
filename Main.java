df5f3571cae1b4a58751e947126f0dfa1386d0f5
Ashi105 <51735758+Ashi105@users.noreply.github.com>
2019-08-04 20:37:45 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,24 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class Main {
-
-public static void main(String[] args) {
-
-String fileName = "data.csv";
-
-File file = new File(fileName);
-try{
- Scanner inputStream = new Scanner(file);
-while(inputStream.hasNext()){
-
-String data = inputStream.next();
-System.out.println(data + "***");
-
-String[] values = data.split(",");
-System.out.println(values[4] + "**");
-
-
-}
-inputStream.close();
-} catch(FileNotFoundException e) {
-
-e.printStackTrace();
-}
+    public static void main(String[] args) {
+    BufferedReader br = null;
+    try {
+    	br = new BufferedReader(new FileReader("block_0.csv"));
+    	String[] line;
+    	while((line = br.readLine()) != null) {
+    		System.out.println(line[0]);
+    	}
+    	
+    } catch(IOException e) {
+    	e.printStackTrace();
+    } finally {
+    	try {
+    	br.close();
+    	}
+    	catch(IOException e) {
+        	e.printStackTrace();}
+    }
+}}
